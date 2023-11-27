37e8f63c50df2275e220bc455c54fb3ad00a512e
Jinxy <oriana.emanoil@gmail.com>
2023-11-28 01:10:08 +0200

    Retus 01:09 a.m.

--- src/component/WorkXP.jsx
+++ src/component/WorkXP.jsx
@@ -5,18 +5,20 @@ import github from '/images/github.svg';
 import css from '/images/css3-original.svg';
 import html from '/images/html5.svg';
 import npm from '/images/npm.svg';
+import javascript from '/images/javascript.svg';
+import react from '/images/react.svg';
 
  function WorkXP() {
 
   return (
     <div id='workxp-container' className='container container-cont workxp-container'>
-        <p className='ptitle-p'>Work experience</p>
+        <p className='ptitle-p'>Work Experience</p>
 
         <ul className='workexp-ul'>
           <li>
             <div className='work-exp-ul-div'>
               <span>2023 - present</span>
-              <span>Front-end</span>
+              <span>Front-end Web Developer</span>
             </div>
             <div>
               <span>Remote</span>
@@ -25,8 +27,8 @@ import npm from '/images/npm.svg';
 
           <li>
             <div className='work-exp-ul-div'>
-              <span>2023 - present</span>
-              <span>Front-end</span>
+              <span>2022 - 2023</span>
+              <span>QA Testing</span>
             </div>
             <div>
               <span>Remote</span>
@@ -35,11 +37,31 @@ import npm from '/images/npm.svg';
 
           <li>
             <div className='work-exp-ul-div'>
-              <span>2023 - present</span>
-              <span>Front-end</span>
+              <span>2019 - 2020</span>
+              <span>Critical Support Administrator</span>
             </div>
             <div>
-              <span>Remote</span>
+              <span>Hybrid</span>
+            </div>
+          </li>
+
+          <li>
+            <div className='work-exp-ul-div'>
+              <span>2017 - 2019</span>
+              <span>QA Testing </span>
+            </div>
+            <div>
+              <span>Office</span>
+            </div>
+          </li>
+
+          <li>
+            <div className='work-exp-ul-div'>
+              <span>2016 - 2017</span>
+              <span>QA Testing </span>
+            </div>
+            <div>
+              <span>Office</span>
             </div>
           </li>
 
@@ -51,36 +73,21 @@ import npm from '/images/npm.svg';
         <div className='mywork-ctim-div'>
           <div className='mywork-box-project'>
             <a href='#'>
-              <img className='mywork-project-img' src={nature} alt=''/>
+              <img className='mywork-project-img' src={nature} alt='spn.png'/>
             </a>
             <div className='mywork-exp-sect'>
-            <span>agagasgas asgagsa gasgag asga gsa gasg asgalgkagas agagkasgag as;lasgas agasgas a</span>
+            <span>A platform build for online courses teaching employes to dive Safe on the internet</span>
             <div className='mywork-exp-sect-tehn'>
-            <img src={laravel} alt='Laravel'/>
-            <img src={github} alt='github.svg'/>
-            <img src={css} alt='css3-original.svg'/>
             <img src={html} alt='html5.svg'/>
+            <img src={css} alt='css3-original.svg'/>
+            <img src={javascript} alt='javascript.svg'/>
+            <img src={react} alt='react.svg'/>
+            <img src={laravel} alt='Laravel'/>
             <img src={npm} alt='npm.svg'/>
+
             </div>
 
-            <p>Front end</p>
-            </div>
-          </div>
-
-          <div className='mywork-box-project'>
-            <a href='#'>
-              <img className='mywork-project-img' src={nature} alt=''/>
-            </a>
-            <div className='mywork-exp-sect'>
-            <span>agagasgas asgagsa gasgag asga gsa gasg asgalgkagas agagkasgag as;lasgas agasgas a</span>
-            <div className='mywork-exp-sect-tehn'>
-            <img src={laravel} alt='Laravel'/>
-            <img src={laravel} alt='Laravel'/>
-            <img src={laravel} alt='Laravel'/>
-            <img src={laravel} alt='Laravel'/>
-            </div>
-
-            <p>Front end</p>
+            <p>Front-end</p>
             </div>
           </div>
 
@@ -89,7 +96,7 @@ import npm from '/images/npm.svg';
               <img className='mywork-project-img' src={nature} alt=''/>
             </a>
             <div className='mywork-exp-sect'>
-            <span>agagasgas asgagsa gasgag asga gsa gasg asgalgkagas agagkasgag as;lasgas agasgas a</span>
+            <span>Aici bag blogul de la class iar poza mi-o va da Ionut daca e Dragut </span>
             <div className='mywork-exp-sect-tehn'>
             <img src={laravel} alt='Laravel'/>
             <img src={laravel} alt='Laravel'/>
@@ -97,7 +104,24 @@ import npm from '/images/npm.svg';
             <img src={laravel} alt='Laravel'/>
             </div>
 
-            <p>Front end</p>
+            <p>Front-end</p>
+            </div>
+          </div>
+
+          <div className='mywork-box-project'>
+            <a href='#'>
+              <img className='mywork-project-img' src={nature} alt=''/>
+            </a>
+            <div className='mywork-exp-sect'>
+            <span>Enjoee- Android & Apple App build to Create Events and invite the people you want or need to</span>
+            <div className='mywork-exp-sect-tehn'>
+            <img src={laravel} alt='Laravel'/>
+            <img src={laravel} alt='Laravel'/>
+            <img src={laravel} alt='Laravel'/>
+            <img src={laravel} alt='Laravel'/>
+            </div>
+
+            <p>QA Testing</p>
             </div>
           </div>
 
@@ -107,7 +131,7 @@ import npm from '/images/npm.svg';
               <img className='mywork-project-img' src={nature} alt=''/>
             </a>
             <div className='mywork-exp-sect'>
-            <span>agagasgas asgagsa gasgag asga gsa gasg asgalgkagas agagkasgag as;lasgas agasgas a</span>
+            <span>PAYSCOUT- A payment platform build for transactions</span>
             <div className='mywork-exp-sect-tehn'>
             <img src={laravel} alt='Laravel'/>
             <img src={laravel} alt='Laravel'/>
@@ -115,7 +139,7 @@ import npm from '/images/npm.svg';
             <img src={laravel} alt='Laravel'/>
             </div>
 
-            <p>Front end</p>
+            <p>QA Testing</p>
             </div>
           </div>
 
@@ -124,7 +148,7 @@ import npm from '/images/npm.svg';
               <img className='mywork-project-img' src={nature} alt=''/>
             </a>
             <div className='mywork-exp-sect'>
-            <span>agagasgas asgagsa gasgag asga gsa gasg asgalgkagas agagkasgag as;lasgas agasgas a</span>
+            <span>E.A. GAMES - Creation games on diffrent platforms like XP0, PS3, PS4 & PC</span>
             <div className='mywork-exp-sect-tehn'>
             <img src={laravel} alt='Laravel'/>
             <img src={laravel} alt='Laravel'/>
@@ -132,7 +156,7 @@ import npm from '/images/npm.svg';
             <img src={laravel} alt='Laravel'/>
             </div>
 
-            <p>Front end</p>
+            <p>QA Testing</p>
             </div>
           </div>
         </div>
